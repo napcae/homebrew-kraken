f0dc3d8b3ecbab9b876f2446a925088448e7b002
Patrick Christopher <coffeepac@gmail.com>
2017-08-15 11:11:49 -0700

    update version

--- k2cli.rb
+++ k2cli.rb
@@ -1,8 +1,8 @@
 class K2cli < Formula
   desc "CLI for K2"
   homepage "https://github.com/samsung-cnct/k2cli"
-  url "https://github.com/samsung-cnct/k2cli/releases/download/1.0.6/k2cli_1.0.6_darwin_amd64.tar.gz"
-  sha256 "f1a98fa2e77169fb9cd983c8f882ffc7429afafb84122a5b592eb29054ac0f9a"
+  url "https://github.com/samsung-cnct/k2cli/releases/download/1.0.8/k2cli_1.0.8_darwin_amd64.tar.gz"
+  sha256 "95b98df9ff3e461c7373f19b704635f71e5b9e27c955e8cbfe819c8812128350
 
   depends_on "docker"
 
