be49ca8cca11710f9e085984fcf6817fed326f92
Patrick Christopher <coffeepac@gmail.com>
2017-08-15 12:18:12 -0700

    add trailing quote

--- k2cli.rb
+++ k2cli.rb
@@ -2,7 +2,7 @@ class K2cli < Formula
   desc "CLI for K2"
   homepage "https://github.com/samsung-cnct/k2cli"
   url "https://github.com/samsung-cnct/k2cli/releases/download/1.0.8/k2cli_1.0.8_darwin_amd64.tar.gz"
-  sha256 "95b98df9ff3e461c7373f19b704635f71e5b9e27c955e8cbfe819c8812128350
+  sha256 "95b98df9ff3e461c7373f19b704635f71e5b9e27c955e8cbfe819c8812128350"
 
   depends_on "docker"
 
