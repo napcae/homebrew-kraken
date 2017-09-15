1374d5643bdd6240ac21a2b600bb520be0161bfd
David E Watson <davidewatson@gmail.com>
2017-09-15 14:50:09 -0700

    Update hash to latest release

--- kraken.rb
+++ kraken.rb
@@ -2,7 +2,7 @@ class Kraken < Formula
   desc "Command line interface for kraken"
   homepage "https://github.com/samsung-cnct/kraken"
   url "https://github.com/samsung-cnct/kraken/releases/download/1.1.0/kraken-rel_1.1.0_darwin_amd64.tar.gz"
-  sha256 "f64964c6326bc9bdcde4b1c17e37ad48ae4a606adc582c41b9d6fb0cba427573"
+  sha256 "84356a61d9b6a069e7f0edc72bc7d626f91f43a10865ec90f29cf1adb34ba647"
 
   depends_on "docker"
 
