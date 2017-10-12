a86086696cc39bded113e1c28c69ab1acf41ff03
davidewatson <davidewatson@gmail.com>
2017-10-11 18:30:20 -0700

    Correct URL and hash for 1.2.0 release. (#18)
    * Correct URL for 1.2.0 release.
    
    * Again.

--- kraken.rb
+++ kraken.rb
@@ -2,7 +2,7 @@ class Kraken < Formula
   desc "Command line interface for kraken"
   homepage "https://github.com/samsung-cnct/kraken"
   url "https://github.com/samsung-cnct/kraken/releases/download/1.2.0/kraken_1.2.0_darwin_amd64.tar.gz"
-  sha256 "b77d4148b0b3bbdc3a34b4758dc37fbd5fb67cb8cbade608f719ea80ee0cb35b"
+  sha256 "c204ad337a2648e7075c80ae163b59f927d7558d7506e99ab8312bfcda9f3832"
 
   depends_on "docker"
 
