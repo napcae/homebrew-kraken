a748984d662ab94b84e2efe1fa1da26cd3bc68d3
Patrick Christopher <coffeepac@gmail.com>
2017-10-24 10:23:37 -0700

    update to version 1.2.2

--- kraken.rb
+++ kraken.rb
@@ -2,8 +2,8 @@ class Kraken < Formula
   desc "Command line interface for kraken"
   homepage "https://github.com/samsung-cnct/kraken"
 
-  url "https://github.com/samsung-cnct/kraken/releases/download/1.2.1/kraken-rel_1.2.1_darwin_amd64.tar.gz"
-  sha256 "18d46c785f406d6e7a9109f745547cacf1dca2fe43b0e994382e55abdc208668"
+  url "https://github.com/samsung-cnct/kraken/releases/download/1.2.2/kraken_1.2.2_darwin_amd64.tar.gz"
+  sha256 "7c5d6aec244384fbb9d43ee212ae950fca9ed6e20254159eed8a485cc0e7fe5c"
 
   depends_on "docker"
 
